fe11bcd6070a76f15c1de4631c119cf394217a21
Lucas Ferreira <ljnferreira@gmail.com>
2022-03-10 12:12:16 -0300

    feat(seeds.rb): added users seed

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+User.create([
+  {
+    email: "ljnferreira@gmail.com",
+    password: "lucas@crawler",
+    username: "Lucas Ferreira",
+    role: "admin"
+  },
+  {
+    email: "armando@inova.com",
+    password: "armando@inova",
+    username: "Armando Assunção",
+    role: "admin"
+  },
+  {
+    email: "richarson@inova.com",
+    password: "richardson@inova",
+    username: "Richardson",
+  }
+])
